c0be4e24736fbb2996da7137aa544e0fb98dab05
ferston <ferso_adidas@msn.com>
2022-04-08 08:51:37 -0500

    prueba pantalla Lg

--- Smart_TV/src/Marcas/Lg.java
+++ Smart_TV/src/Marcas/Lg.java
@@ -3,4 +3,15 @@ package Marcas;
 import SO_TV.WebOs;
 
 public class Lg extends WebOs {
+    String menu;
+
+    public Lg(int pulgadas, String marca, String SO, Boolean HDMI, Boolean HDR, String definicion, String menu) {
+        super(pulgadas, marca, SO, HDMI, HDR, definicion);
+        this.menu = menu;
+    }
+
+    public void MostrarEspecificaciones(){
+        System.out.println("Marca: "+ getMarca() + "pualgadas: "+ 45 );
+
+    }
 }
---
+++ Smart_TV/src/smart_tv/Prueba_Smart_tv.java
@@ -0,0 +1,10 @@
+package smart_tv;
+
+import Marcas.Lg;
+
+public class Prueba_Smart_tv {
+    public static void main(String[] args) {
+Lg lg1 = new Lg(45,"Lg","WebOs",true,false,"4k","Menu Lg");
+lg1.MostrarEspecificaciones();
+    }
+}
