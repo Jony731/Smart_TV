814102d078b4c7c31c27d6d44f54581a5ce505bb
ferston <ferso_adidas@msn.com>
2022-04-11 22:51:17 -0500

    prueba

--- Smart_TV/src/smart_tv/Lista_Smart_Tvs.java
+++ Smart_TV/src/smart_tv/Lista_Smart_Tvs.java
@@ -9,7 +9,7 @@ public class Lista_Smart_Tvs {
     public void crearLista(){
         ArrayList<Lg> ListaLG = new ArrayList<>();
         ListaLG.add(1,new Lg(45,"Lg","WebOs",true,false,"4k","Menu Lg Web OS"));
-
+//hola
     }
 
 
